7977ecbe0a44cbcfdbfd18f0cb409cbb09f0a924
R0L4 <52117091+R0L4@users.noreply.github.com>
2019-07-09 14:05:59 +0700

    Add files via upload

---
+++ py.sh
@@ -0,0 +1,56 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+dark='\033[90;1m'
+clear
+echo $indigo
+date
+echo
+#!/bin/sh
+
+echo $red""
+echo " _____ ____   ___      _   _    _   _ "
+sleep 0.1
+echo "|_   _|  _ \ / _ \    | | / \  | \ | |"
+sleep 0.1
+echo "  | | | |_) | | | |_  | |/ _ \ |  \| |"
+sleep 0.1
+echo "  | | |  _ <| |_| | |_| / ___ \| |\  |"
+sleep 0.1
+echo "  |_| |_| \_|\___/ \___/_/   \_\_| \_|    v1.2"
+sleep 0.5
+
+#credit
+sleep 0.1
+echo '\033[34;1m'==========================================
+sleep 0.1
+echo '\033[33;1m'"=            TROJAN WIN32                ="
+sleep 0.1
+echo '\033[34;1m'==========================================
+sleep 0.1
+
+#credit
+sleep 0.1
+echo $green"=========================================="
+sleep 0.1
+echo '\033[34;1m'= AUTHOR : '\033[33;1m'MR.R0L4           '\033[34;1m'=
+sleep 0.1
+echo '\033[34;1m'= Support : '\033[33;1m'MR.3MB3P, MR.MASK        '\033[34;1m'=
+sleep 0.1
+echo '\033[34;1m'= TEAM : '\033[33;1m'O.H.C.T             '\033[34;1m'=
+sleep 0.1
+echo $green"=========================================="
+
+#attack
+sleep 0.1
+echo $red"Masukan IP Target"
+sleep 0.1
+echo $cyan""
+sleep 0.1
+read -p "[Ip Target]==> " ip
+sleep 0.5
+python3 win32.py -s $ip -p 80 -t 135                                                                                                         fi
