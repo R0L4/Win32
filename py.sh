a8d1d12e63f3a4a57f17e3c1d18b74c57641e0e0
R0L4 <52117091+R0L4@users.noreply.github.com>
2019-07-12 17:06:21 +0700

    Update py.sh

--- py.sh
+++ py.sh
@@ -39,7 +39,8 @@ echo $green"=========================================="
 sleep 0.1
 echo '\033[34;1m'= AUTHOR : '\033[33;1m'MR.R0L4           '\033[34;1m'=
 sleep 0.1
-echo '\033[34;1m'= Support : '\033[33;1m'MR.3MB3P, MR.MASK        '\033[34;1m'=
+echo '\033[34;1m'= Support : '\033[33;1m'MR.3MB3P, MR.N0ob109
+        '\033[34;1m'=
 sleep 0.1
 echo '\033[34;1m'= TEAM : '\033[33;1m'O.H.C.T             '\033[34;1m'=
 sleep 0.1
